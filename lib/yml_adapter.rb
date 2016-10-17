0c5290373e5917b1b2818e686a490788e79b9776
Jakub Niewczas <niewczas.jakub@gmail.com>
2016-10-18 00:51:19 +0800

    unify adapters api

--- lib/yml_adapter.rb
+++ lib/yml_adapter.rb
@@ -1,6 +1,14 @@
 class YmlAdapter
-  def load_data(resource, language:)
-    data = YAML::load_file("db/#{resource}.yml")
+  def load_data(resources_name, language:)
+    data = YAML::load_file("db/#{resources_name}.yml")
     data[language.to_s]
+  end
+
+  def clear(resources_name)
+    raise NotImplementedError
+  end
+
+  def add_data(resources_name, data:)
+    raise NotImplementedError
   end
 end
