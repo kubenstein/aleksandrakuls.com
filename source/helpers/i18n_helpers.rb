6aedb24837336748776aed8b4fa3dc56e1ca95a8
Jakub Niewczas <niewczas.jakub@gmail.com>
2016-10-23 23:54:37 +0800

    rename param to avoid _

--- source/helpers/i18n_helpers.rb
+++ source/helpers/i18n_helpers.rb
@@ -6,7 +6,7 @@ def current_lang
   I18n.locale
 end
 
-def localized_path(path, lang=current_lang, _default_lang=default_lang)
+def localized_path(path, lang=current_lang, default_language=default_lang)
   route = path.gsub("#{current_lang}/", '')
-  lang == _default_lang ? route : "/#{lang}#{route}"
+  lang == default_language ? route : "/#{lang}#{route}"
 end
